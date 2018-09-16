47260f516176503b0e19b7a9cd144dc2c81a7e54
John Lombard <John@Johns-MBP.fios-router.home>
2018-09-15 21:33:15 -0400

    linking stylesheets and testing the IEX API

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("hello");
+// on click of the submit button gathers information selected in the search bar
+$("#submit").on("click", function(event) {
+    console.log(0);
+event.preventDefault()
+
+var searchTerm = $("#search").val().trim();
+console.log(searchTerm);
+
+}
+
+
+
+
+
+);
+
+
+
+
+  function grabURL () {
+    var ticker = "aapl";  
+    var queryURL = "https://api.iextrading.com/1.0/stock/" + ticker + "/batch?types=quote,news,chart&range=1m&last=10";
+      
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response)
+        console.log(response.quote.symbol)
+        console.log(response.quote.companyName)
+      });
+    
+  }
+
+  grabURL();
