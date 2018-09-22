3deb282694416f80827c9b3fb4bf8da769d2f925
Adam Stevens <astevens926@gmail.com>
2018-09-22 10:17:40 -0400

    changing colors if positive/negative

--- script.js
+++ script.js
@@ -62,9 +62,21 @@ function companyData(response) {
 
     //amount change
     $("#priceChange").text("Price Change:  " + response.quote.change);
+    if (Math.sign(response.quote.change) === -1) {
+        $("#priceChange").css("color", "red");
+    }
+    else {
+        $("#priceChange").css("color", "green");
+    }
 
     //percent change 
     $("#percentChange").text("Percent Change:  " + rounding(response.quote.changePercent * 100) + "%");
+    if (Math.sign(response.quote.changePercent) === -1) {
+        $("#percentChange").css("color", "red");
+    }
+    else {
+        $("#percentChange").css("color", "green");
+    }
 };
 
 // Add Logo
